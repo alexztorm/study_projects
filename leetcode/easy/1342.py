3ac6224791813082738977030d4f5d1a23b784e5
alexztorm <jr_wasp@mail.ru>
2024-09-26 07:09:51 +0300

    mod 1342.py

--- leetcode/easy/1342.py
+++ leetcode/easy/1342.py
@@ -12,7 +12,20 @@ class Solution:
         return ans
 
 
+    def number_of_steps2(self, num: int) -> int:
+        ans = 0
+
+        while num != 0:
+            while num & 1 == 0:
+                ans += 1
+                num = num >> 1
+            ans += 1
+            num -= 1
+
+        return ans
+
+
 s = Solution()
-print(s.number_of_steps(14))
-print(s.number_of_steps(8))
-print(s.number_of_steps(123))
+print(s.number_of_steps2(14))
+print(s.number_of_steps2(8))
+print(s.number_of_steps2(123))
