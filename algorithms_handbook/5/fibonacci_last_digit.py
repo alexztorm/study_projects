93a07adfe16d946cd162111e3b084ff9a699025b
alexztorm <jr_wasp@mail.ru>
2024-09-27 08:02:27 +0300

    add fibonacci_last_digit.py

---
+++ algorithms_handbook/5/fibonacci_last_digit.py
@@ -0,0 +1,19 @@
+def fibonacci_last_digit(n: int) -> int:
+    if n <= 1:
+        return n
+    else:
+        old_prev = 0
+        prev = 1
+
+        for i in range(n - 1):
+            tmp = old_prev
+            old_prev = prev
+            prev += tmp
+            prev = prev % 10
+
+        return prev
+
+
+m = int(input())
+
+print(fibonacci_last_digit(m))
