30d1f154d77cde18911ff7a80a8a0b3c0ea0e79d
alexztorm <jr_wasp@mail.ru>
2024-09-29 17:38:18 +0300

    started task_2.py

---
+++ university_hw/third_term/reliability_models/task_2.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.canvas_list = []
+        self.button_list = []
+
+        new_canvas = tk.Canvas(self, width=250, height=130)
+        new_canvas.grid(row=0, column=0)
+        self.canvas_list.append(new_canvas)
+        # self.img = ImageTk.PhotoImage(Image.open("schemes/scheme_1.png"))
+        # canvas.create_image(5, 5, anchor="nw", image=self.img)
+        new_button = tk.Button(self, text="Запустить расчет")
+        new_button.grid(row=2, column=0)
+        self.button_list.append(new_button)
+        new_button = tk.Button(self, text="Добавить", command=self.add_scheme_element)
+        new_button.grid(row=1, column=0)
+        self.button_list.append(new_button)
+
+    def add_scheme_element(self):
+        ...
+
+
+m = MainWindow()
+tk.mainloop()
