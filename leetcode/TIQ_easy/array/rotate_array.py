746657968714dd85e128a092680d855c7a7eb5f1
alexztorm <jr_wasp@mail.ru>
2024-10-06 10:06:55 +0300

    add rotate_2 to rotate_array.py

--- leetcode/TIQ_easy/array/rotate_array.py
+++ leetcode/TIQ_easy/array/rotate_array.py
@@ -15,5 +15,19 @@ def rotate(nums: list[int], k: int) -> None:
     print(nums)
 
 
+def rotate_2(nums: list[int], k: int) -> None:
+    n = len(nums)
+
+    if k >= n:
+        k = k % n
+
+    if k != 0:
+        nums[:k], nums[k:] = nums[-k:], nums[:-k]
+    print(nums)
+
+
 rotate([1, 2, 3, 4, 5, 6, 7], 3)
 rotate([-1, -100, 3, 99], 2)
+rotate_2([1, 2, 3, 4, 5, 6, 7], 3)
+rotate_2([-1, -100, 3, 99], 2)
+rotate_2([1], 0)
