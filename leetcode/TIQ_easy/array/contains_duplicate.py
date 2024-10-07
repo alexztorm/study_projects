bec0a8b6ebf04588903025a5ad7337d02d0b50e6
alexztorm <jr_wasp@mail.ru>
2024-10-07 07:44:32 +0300

    add containsDuplicate2 to contains_duplicate.py

--- leetcode/TIQ_easy/array/contains_duplicate.py
+++ leetcode/TIQ_easy/array/contains_duplicate.py
@@ -5,6 +5,13 @@ def containsDuplicate(nums: list[int]) -> bool:
         return True
 
 
-print(containsDuplicate([1, 2, 3, 1]))
-print(containsDuplicate([1, 2, 3, 4]))
-print(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+def containsDuplicate2(nums: list[int]) -> bool:
+    for i in range(len(nums)):
+        if nums[i] in nums[:i]:
+            return True
+    return False
+
+
+print(containsDuplicate2([1, 2, 3, 1]))
+print(containsDuplicate2([1, 2, 3, 4]))
+print(containsDuplicate2([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
