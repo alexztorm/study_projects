6e948db3520656ae3c5449a7577a3710bf319958
alexztorm <jr_wasp@mail.ru>
2024-10-17 21:38:46 +0300

    add spice.py (not finished)

---
+++ algorithms_handbook/chapter_6/spice.py
@@ -0,0 +1,31 @@
+def max_spice(w: int, weight: list[int], cost: list[int]) -> float:
+    if w == 0 or not weight:
+        return 0
+
+    max_cost, max_cost_id = 0, 0
+
+    for i in range(len(cost)):
+        if max_cost < (cost[i] / weight[i]):
+            max_cost = cost[i] / weight[i]
+            max_cost_id = i
+
+    amount = min(weight[max_cost_id], w)
+
+    revenue = amount * max_cost
+
+    weight.pop(max_cost_id)
+    cost.pop(max_cost_id)
+
+    return revenue + max_spice(w - amount, weight, cost)
+
+
+n, cap = map(int, input().split())
+
+weight_list, cost_list = [], []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    weight_list.append(b)
+    cost_list.append(a)
+
+print("{:.3f}".format(max_spice(cap, weight_list, cost_list)))
