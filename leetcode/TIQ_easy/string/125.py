743d8e0fdd66d920a3abdf36173bf5e94d174758
alexztorm <jr_wasp@mail.ru>
2024-10-20 08:51:47 +0300

    add isPalindrome2 to 125.py

--- leetcode/TIQ_easy/string/125.py
+++ leetcode/TIQ_easy/string/125.py
@@ -21,9 +21,25 @@ def isPalindrome(s: str) -> bool:
 
     return True
 
+def isPalindrome2(s: str) -> bool:
+    left, right = 0, len(s) - 1
 
-print(isPalindrome("A man, a plan, a canal: Panama"))
-print(isPalindrome("race a car"))
-print(isPalindrome("!!!"))
-print(isPalindrome(" "))
-print(isPalindrome("Marge, let's \"[went].\" I await {news} telegram."))
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+        elif not s[right].isalnum():
+            right -= 1
+        elif s[left].lower() != s[right].lower():
+            return False
+        else:
+            left += 1
+            right -= 1
+
+    return True
+
+
+print(isPalindrome2("A man, a plan, a canal: Panama"))
+print(isPalindrome2("race a car"))
+print(isPalindrome2("!!!"))
+print(isPalindrome2(" "))
+print(isPalindrome2("Marge, let's \"[went].\" I await {news} telegram."))
