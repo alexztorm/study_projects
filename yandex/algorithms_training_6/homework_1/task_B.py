3bc7a119c18d8619e99c14118ca1256f00a8725d
alexztorm <jr_wasp@mail.ru>
2024-10-25 10:37:43 +0300

    mod task_B.py (input)

--- yandex/algorithms_training_6/homework_1/task_B.py
+++ yandex/algorithms_training_6/homework_1/task_B.py
@@ -16,15 +16,15 @@ def shirts_n_socks(a: int, b: int, c: int, d: int):
         return b + 1, d + 1
 
 
-# input_list = []
+input_list = []
 
-# for i in range(4):
-#     input_list.append(int(input()))
+for i in range(4):
+    input_list.append(int(input()))
 
-# x, y = shirts_n_socks(input_list[0], input_list[1], input_list[2], input_list[3])
-# print(x, y)
+x, y = shirts_n_socks(input_list[0], input_list[1], input_list[2], input_list[3])
+print(x, y)
 
-for i in range(100):
-    test = sample(range(10), 4)
-    x, y = shirts_n_socks(test[0], test[1], test[2], test[3])
-    print(test, x, y)
+# for i in range(100):
+#   test = sample(range(10), 4)
+#    x, y = shirts_n_socks(test[0], test[1], test[2], test[3])
+#    print(test, x, y)
