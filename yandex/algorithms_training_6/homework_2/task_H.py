4d76972edb382ae17ce56901fef2a67184cc563d
alexztorm <jr_wasp@mail.ru>
2024-11-03 09:17:39 +0300

    add task_H.py (unfinished)

---
+++ yandex/algorithms_training_6/homework_2/task_H.py
@@ -0,0 +1,11 @@
+def move_to_openspace(n: int, numbers: list[int]) -> int:
+    ...
+
+
+# m = int(input())
+# nums = [int(x) for x in input().split()]
+
+# print(move_to_openspace(m, nums))
+
+print(move_to_openspace(4, [5, 2, 3, 1]))
+print(move_to_openspace(5, [5, 4, 3, 2, 1]))
