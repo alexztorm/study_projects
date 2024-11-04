3a1d1474ed97bd637c6f3128921695c6f5150e9b
alexztorm <jr_wasp@mail.ru>
2024-11-04 09:28:56 +0300

    mod task_F.py (solved)

--- yandex/algorithms_training_6/homework_2/task_F.py
+++ yandex/algorithms_training_6/homework_2/task_F.py
@@ -3,13 +3,20 @@ def sum_of_multiplications_3(n: int, numbers: list[int]) -> int:
         return (numbers[0] * numbers[1] * numbers[2]) % 1000000007
     else:
         mult_sum = 0
-        right = 2
 
-        while right < n:
-            mult_sum += numbers[right - 2] * numbers[right - 1] * numbers[right]
-            right += 1
+        pref_sum = [0]
+
+        for i in range(n):
+            pref_sum.append(pref_sum[i] + numbers[i])
+
+        for i in range(1, n - 1):
+            mult_sum += calc_prefix_sum(0, i - 1, pref_sum) * numbers[i] * calc_prefix_sum(i + 1, n - 1, pref_sum)
 
         return mult_sum % 1000000007
+
+
+def calc_prefix_sum(l: int, r: int, prefix_sum: list[int]) -> int:
+    return prefix_sum[r + 1] - prefix_sum[l]
 
 
 # m = int(input())
@@ -20,4 +27,5 @@ def sum_of_multiplications_3(n: int, numbers: list[int]) -> int:
 print(sum_of_multiplications_3(3, [1, 2, 3]))
 print(sum_of_multiplications_3(4, [0, 5, 6, 7]))
 print(sum_of_multiplications_3(4, [10**6, 10**5 + 1, 10**5 + 3, 7]))
-print(sum_of_multiplications_3(5, [10, 6, 10, 3, 7]))
+print(sum_of_multiplications_3(5, [10, 6, 10, 3, 7]))   # test 3
+print(sum_of_multiplications_3(3, [143461, 574468, 902994]))   # test 7
