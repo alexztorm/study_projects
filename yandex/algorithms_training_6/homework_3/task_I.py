fe97103e6a50420005162319b7f3677bfaa75928
alexztorm <jr_wasp@mail.ru>
2024-11-12 16:09:42 +0300

    mod task_I.py (unfinished)

--- yandex/algorithms_training_6/homework_3/task_I.py
+++ yandex/algorithms_training_6/homework_3/task_I.py
@@ -1,15 +1,13 @@
 def crossroad(n: int, mr1: int, mr2: int, dist: list[int], time: list[int]) -> list[int]:
     res = [0] * n
 
-    queues = [[], [], [], []]
+    queues = {1: [], 2: [], 3: [], 4: []}
 
     ids = list(range(0, n))
     dist_time = sorted(zip(dist, time, ids), key=lambda x: x[1])
 
-    print(dist_time)
-
     for rover in dist_time:
-        queues[rover[0] - 1].append(rover)
+        queues[rover[0]].append(rover)
 
     if abs(mr2 - mr1) == 2:
         straight_road = True
@@ -18,55 +16,76 @@ def crossroad(n: int, mr1: int, mr2: int, dist: list[int], time: list[int]) -> l
 
     nmr1, nmr2 = -1, -1
     for i in range(4):
-        if mr1 != i + 1 or mr2 != i + 1:
-            ...
-    
+        if mr1 != i + 1 and mr2 != i + 1:
+            if nmr1 == -1:
+                nmr1 = i + 1
+            else:
+                nmr2 = i + 1
+
     cur_time = min(time) - 1
 
-    while queues[0] or queues[1] or queues[2] or queues[3]:
-        break
+    while queues[1] or queues[2] or queues[3] or queues[4]:
         cur_time += 1
         if straight_road:
-            if queues[mr1 - 1] or queues[mr2 - 1]:
-                if queues[mr1 - 1]:
-                    res[queues[mr1 - 1][0][2]] = cur_time
-                    queues[mr1 - 1].pop(0)
-                if queues[mr2 - 1]:
-                    res[queues[mr2 - 1][0][2]] = cur_time
-                    queues[mr2 - 1].pop(0)
+            if queues[mr1] and queues[mr1][0][1] <= cur_time or queues[mr2] and queues[mr2][0][1] <= cur_time:
+                if queues[mr1] and queues[mr1][0][1] <= cur_time:
+                    res[queues[mr1][0][2]] = cur_time
+                    queues[mr1].pop(0)
+                if queues[mr2] and queues[mr2][0][1] <= cur_time:
+                    res[queues[mr2][0][2]] = cur_time
+                    queues[mr2].pop(0)
             else:
-                ...
+                if queues[nmr1] and queues[nmr1][0][1] <= cur_time:
+                    res[queues[nmr1][0][2]] = cur_time
+                    queues[nmr1].pop(0)
+                if queues[nmr2] and queues[nmr2][0][1] <= cur_time:
+                    res[queues[nmr2][0][2]] = cur_time
+                    queues[nmr2].pop(0)
         else:
-            ...
+            if queues[mr1] and queues[mr1][0][1] <= cur_time or queues[mr2] and queues[mr2][0][1] <= cur_time:
+                if queues[mr1] and queues[mr1][0][1] <= cur_time and (mr1 < mr2 or mr1 == 4 and mr2 == 1):
+                    res[queues[mr1][0][2]] = cur_time
+                    queues[mr1].pop(0)
+                elif queues[mr2] and queues[mr2][0][1] <= cur_time:
+                    res[queues[mr2][0][2]] = cur_time
+                    queues[mr2].pop(0)
+            else:
+                if queues[nmr1] and queues[nmr1][0][1] <= cur_time and (nmr1 < nmr2 or nmr1 == 4 and nmr2 == 1):
+                    res[queues[nmr1][0][2]] = cur_time
+                    queues[nmr1].pop(0)
+                elif queues[nmr2] and queues[nmr2][0][1] <= cur_time:
+                    res[queues[nmr2][0][2]] = cur_time
+                    queues[nmr2].pop(0)
 
     return res
 
 
-# m = int(input())
-# m1, m2 = map(int, input().split())
-#
-# d, t = [], []
-# for _ in range(m):
-#     a, b = map(int, input().split())
-#     d.append(a)
-#     t.append(a)
-#
-# ans = crossroad(m, m1, m2, d, t)
+m = int(input())
+m1, m2 = map(int, input().split())
 
-# for item in ans:
-#     print(item)
+d, t = [], []
+for _ in range(m):
+    a, b = map(int, input().split())
+    d.append(a)
+    t.append(b)
 
-print(crossroad(4, 1, 3, [1, 3, 2, 2], [1, 1, 1, 2]))   #
-print(crossroad(4, 1, 3, [1, 2, 3, 2], [1, 1, 1, 2]))   #
-print(crossroad(4, 1, 2, [1, 2, 3, 4], [1, 1, 1, 1]))   #
-print(crossroad(3, 1, 2, [1, 3, 4], [2, 1, 2]))         #
-print(crossroad(4, 1, 3, [1, 3, 2, 2], [1, 1, 7, 8]))   # 1, 1, 7, 8
-print(crossroad(2, 1, 3, [2, 2], [1, 1]))   # 1, 2
-print(crossroad(2, 1, 2, [2, 1], [1, 7]))   # 1, 7
-print(crossroad(2, 1, 2, [2, 1], [1, 1]))   # 2, 1
-print(crossroad(4, 1, 3, [2, 4, 2, 4], [1, 1, 1, 1]))   # 1, 1, 2, 2
-print(crossroad(4, 1, 3, [1, 3, 1, 3], [2, 2, 1, 1]))   # 2, 2, 1, 1
-print(crossroad(1, 1, 3, [2], [95]))   # 95
-print(crossroad(2, 1, 3, [2, 4], [95, 107]))   # 95, 107
-print(crossroad(2, 1, 3, [2, 4], [107, 95]))   # 107, 95
-print(crossroad(4, 4, 1, [4, 1, 1, 2], [1, 1, 4, 1]))   # 1, 2, 4, 3
+ans = crossroad(m, m1, m2, d, t)
+
+for item in ans:
+    print(item)
+
+# print(crossroad(4, 1, 3, [1, 3, 2, 2], [1, 1, 1, 2]))   # 1, 1, 2, 3
+# print(crossroad(4, 1, 3, [1, 2, 3, 2], [1, 1, 1, 2]))   # 1, 2, 1, 3
+# print(crossroad(4, 1, 2, [1, 2, 3, 4], [1, 1, 1, 1]))   # 1, 2, 3, 4
+# print(crossroad(3, 1, 2, [1, 3, 4], [2, 1, 2]))         # 2, 1, 3
+# print(crossroad(4, 1, 3, [1, 3, 2, 2], [1, 1, 7, 8]))   # 1, 1, 7, 8
+# print(crossroad(2, 1, 3, [2, 2], [1, 1]))   # 1, 2
+# print(crossroad(2, 1, 2, [2, 1], [1, 7]))   # 1, 7
+# print(crossroad(2, 1, 2, [2, 1], [1, 1]))   # 2, 1
+# print(crossroad(4, 1, 3, [2, 4, 2, 4], [1, 1, 1, 1]))   # 1, 1, 2, 2
+# print(crossroad(4, 1, 3, [1, 3, 1, 3], [2, 2, 1, 1]))   # 2, 2, 1, 1
+# print(crossroad(1, 1, 3, [2], [95]))   # 95
+# print(crossroad(2, 1, 3, [2, 4], [95, 107]))   # 95, 107
+# print(crossroad(2, 1, 3, [2, 4], [107, 95]))   # 107, 95
+# print(crossroad(4, 4, 1, [4, 1, 1, 2], [1, 1, 4, 1]))   # 1, 2, 4, 3
+# print(crossroad(4, 1, 3, [1, 2, 3, 4], [1, 2, 2, 3]))
