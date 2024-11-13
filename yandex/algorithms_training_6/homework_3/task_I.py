5ec3a2fd4b81fb9cf95d5b9cb38270964c48fbfb
alexztorm <jr_wasp@mail.ru>
2024-11-13 20:28:44 +0300

    mod task_I.py (solved)

--- yandex/algorithms_training_6/homework_3/task_I.py
+++ yandex/algorithms_training_6/homework_3/task_I.py
@@ -6,56 +6,65 @@ def crossroad(n: int, mr1: int, mr2: int, dist: list[int], time: list[int]) -> l
     ids = list(range(0, n))
     dist_time = sorted(zip(dist, time, ids), key=lambda x: x[1])
 
-    for rover in dist_time:
-        queues[rover[0]].append(rover)
+    for item in dist_time:
+        queues[item[0]].append(item)
 
-    if abs(mr2 - mr1) == 2:
-        straight_road = True
-    else:
-        straight_road = False
+    cur_time = -1
+    count_rovers = 0
+    straight_road = (abs(mr1 - mr2) == 2)
+    nmr1, nmr2 = 5 - mr2, 5 - mr1
 
-    nmr1, nmr2 = -1, -1
-    for i in range(4):
-        if mr1 != i + 1 and mr2 != i + 1:
-            if nmr1 == -1:
-                nmr1 = i + 1
-            else:
-                nmr2 = i + 1
-
-    cur_time = min(time) - 1
-
-    while queues[1] or queues[2] or queues[3] or queues[4]:
-        cur_time += 1
-        if straight_road:
-            if queues[mr1] and queues[mr1][0][1] <= cur_time or queues[mr2] and queues[mr2][0][1] <= cur_time:
-                if queues[mr1] and queues[mr1][0][1] <= cur_time:
+    if straight_road:
+        while count_rovers < n:
+            a, b = queues[mr1] and queues[mr1][0][1] <= cur_time, queues[mr2] and queues[mr2][0][1] <= cur_time
+            if a or b:
+                if a:
                     res[queues[mr1][0][2]] = cur_time
                     queues[mr1].pop(0)
-                if queues[mr2] and queues[mr2][0][1] <= cur_time:
+                    count_rovers += 1
+                if b:
                     res[queues[mr2][0][2]] = cur_time
                     queues[mr2].pop(0)
+                    count_rovers += 1
             else:
                 if queues[nmr1] and queues[nmr1][0][1] <= cur_time:
                     res[queues[nmr1][0][2]] = cur_time
                     queues[nmr1].pop(0)
+                    count_rovers += 1
                 if queues[nmr2] and queues[nmr2][0][1] <= cur_time:
                     res[queues[nmr2][0][2]] = cur_time
                     queues[nmr2].pop(0)
+                    count_rovers += 1
+            cur_time += 1
+    else:
+        mult = mr1 * mr2
+        if mult == 2:
+            prior = [1, 2, 3, 4]
+        elif mult == 4:
+            prior = [4, 1, 2, 3]
+        elif mult == 6:
+            prior = [2, 3, 4, 1]
         else:
-            if queues[mr1] and queues[mr1][0][1] <= cur_time or queues[mr2] and queues[mr2][0][1] <= cur_time:
-                if queues[mr1] and queues[mr1][0][1] <= cur_time and (mr1 < mr2 or mr1 == 4 and mr2 == 1):
-                    res[queues[mr1][0][2]] = cur_time
-                    queues[mr1].pop(0)
-                elif queues[mr2] and queues[mr2][0][1] <= cur_time:
-                    res[queues[mr2][0][2]] = cur_time
-                    queues[mr2].pop(0)
-            else:
-                if queues[nmr1] and queues[nmr1][0][1] <= cur_time and (nmr1 < nmr2 or nmr1 == 4 and nmr2 == 1):
-                    res[queues[nmr1][0][2]] = cur_time
-                    queues[nmr1].pop(0)
-                elif queues[nmr2] and queues[nmr2][0][1] <= cur_time:
-                    res[queues[nmr2][0][2]] = cur_time
-                    queues[nmr2].pop(0)
+            prior = [3, 4, 1, 2]
+
+        while count_rovers < n:
+            if queues[prior[0]] and queues[prior[0]][0][1] <= cur_time:
+                res[queues[prior[0]][0][2]] = cur_time
+                queues[prior[0]].pop(0)
+                count_rovers += 1
+            elif queues[prior[1]] and queues[prior[1]][0][1] <= cur_time:
+                res[queues[prior[1]][0][2]] = cur_time
+                queues[prior[1]].pop(0)
+                count_rovers += 1
+            elif queues[prior[2]] and queues[prior[2]][0][1] <= cur_time:
+                res[queues[prior[2]][0][2]] = cur_time
+                queues[prior[2]].pop(0)
+                count_rovers += 1
+            elif queues[prior[3]] and queues[prior[3]][0][1] <= cur_time:
+                res[queues[prior[3]][0][2]] = cur_time
+                queues[prior[3]].pop(0)
+                count_rovers += 1
+            cur_time += 1
 
     return res
 
@@ -88,4 +97,4 @@ for item in ans:
 # print(crossroad(2, 1, 3, [2, 4], [95, 107]))   # 95, 107
 # print(crossroad(2, 1, 3, [2, 4], [107, 95]))   # 107, 95
 # print(crossroad(4, 4, 1, [4, 1, 1, 2], [1, 1, 4, 1]))   # 1, 2, 4, 3
-# print(crossroad(4, 1, 3, [1, 2, 3, 4], [1, 2, 2, 3]))
+# print(crossroad(4, 1, 3, [1, 2, 3, 4], [1, 2, 2, 3]))   # 1, 3, 2, 3
