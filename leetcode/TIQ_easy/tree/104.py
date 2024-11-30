b0c394c40ab1a27bc0e347797c03942c0bcea69d
alexztorm <jr_wasp@mail.ru>
2024-11-30 09:37:52 +0300

    add 104.py

---
+++ leetcode/TIQ_easy/tree/104.py
@@ -0,0 +1,17 @@
+def maxDepth(root) -> int:
+    max_depth, cur_depth = 0, 1
+    nodes_stack = []
+    cur_node = root
+
+    while nodes_stack or cur_node:
+        if cur_node:
+            nodes_stack.append((cur_node, cur_depth))
+            cur_node = cur_node.left
+            cur_depth += 1
+        elif nodes_stack:
+            tmp = nodes_stack.pop()
+            max_depth = max(max_depth, tmp[1])
+            cur_node = tmp[0].right
+            cur_depth = tmp[1] + 1
+
+    return max_depth
