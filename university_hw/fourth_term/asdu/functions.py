2983f4f871d70055733d2d4bd7419a97b11ead0c
alexztorm <jr_wasp@mail.ru>
2025-04-19 21:25:27 +0300

    asdu lab small fix

--- university_hw/fourth_term/asdu/functions.py
+++ university_hw/fourth_term/asdu/functions.py
@@ -73,7 +73,7 @@ def animate_non_stationary(l: float, time_limit: int, p_start: float, p_end: flo
     ax1.grid(True)
 
     ax2.set_xlim(0, l / 1000)
-    ax2.set_ylim(0.0, 10.0)
+    ax2.set_ylim(-10.0, 10.0)
     ax2.set_xlabel('Расстояние, км')
     ax2.set_ylabel('Скорость, м/с')
     ax2.grid(True)
